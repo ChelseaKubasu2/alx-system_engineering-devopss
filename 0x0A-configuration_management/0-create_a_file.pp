eeb620c0523da3c9d70042ca320fbe5765c92396
Kubasu Chelsea <98890263+ChelseaKubasu2@users.noreply.github.com>
2022-09-21 11:24:46 +0300

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#creating file
-
-file { 'tmp/school':
-   ensure  => 'file',
-   content => 'I love Puppet',
-   mode    => '0744',
-   owner   => 'www-data',
-   group   => 'www-data',
-  }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# create a manifest that kills a process ~killmenow
-
-exec {'pkill killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/',
-}
-Footer
-© 2022 GitHub, Inc.
